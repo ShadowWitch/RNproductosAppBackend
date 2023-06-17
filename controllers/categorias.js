564a696c36d613a6e39f9af453a71bc3293340d2
YafethMC <lymoya@unah.hn>
2023-06-16 18:52:49 -0600

    qweqweqwe

--- controllers/categorias.js
+++ controllers/categorias.js
@@ -2,17 +2,19 @@ const { response } = require('express');
 const { Categoria } = require('../models');
 
 
-const obtenerCategorias = async(req, res = response ) => {
+const obtenerCategorias = async (req, res = response) => {
 
     const { limite = 5, desde = 0 } = req.query;
     const query = { estado: true };
 
-    const [ total, categorias ] = await Promise.all([
+    console.log('PERICION >> ', req.query);
+
+    const [total, categorias] = await Promise.all([
         Categoria.countDocuments(query),
         Categoria.find(query)
             .populate('usuario', 'nombre')
-            .skip( Number( desde ) )
-            .limit(Number( limite ))
+            .skip(Number(desde))
+            .limit(Number(limite))
     ]);
 
     res.json({
@@ -21,25 +23,25 @@ const obtenerCategorias = async(req, res = response ) => {
     });
 }
 
-const obtenerCategoria = async(req, res = response ) => {
+const obtenerCategoria = async (req, res = response) => {
 
     const { id } = req.params;
-    const categoria = await Categoria.findById( id )
-                            .populate('usuario', 'nombre');
+    const categoria = await Categoria.findById(id)
+        .populate('usuario', 'nombre');
 
-    res.json( categoria );
+    res.json(categoria);
 
 }
 
-const crearCategoria = async(req, res = response ) => {
+const crearCategoria = async (req, res = response) => {
 
     const nombre = req.body.nombre.toUpperCase();
 
     const categoriaDB = await Categoria.findOne({ nombre });
 
-    if ( categoriaDB ) {
+    if (categoriaDB) {
         return res.status(400).json({
-            msg: `La categoria ${ categoriaDB.nombre }, ya existe`
+            msg: `La categoria ${categoriaDB.nombre}, ya existe`
         });
     }
 
@@ -49,7 +51,7 @@ const crearCategoria = async(req, res = response ) => {
         usuario: req.usuario._id
     }
 
-    const categoria = new Categoria( data );
+    const categoria = new Categoria(data);
 
     // Guardar DB
     await categoria.save();
@@ -58,26 +60,26 @@ const crearCategoria = async(req, res = response ) => {
 
 }
 
-const actualizarCategoria = async( req, res = response ) => {
+const actualizarCategoria = async (req, res = response) => {
 
     const { id } = req.params;
     const { estado, usuario, ...data } = req.body;
 
-    data.nombre  = data.nombre.toUpperCase();
+    data.nombre = data.nombre.toUpperCase();
     data.usuario = req.usuario._id;
 
     const categoria = await Categoria.findByIdAndUpdate(id, data, { new: true });
 
-    res.json( categoria );
+    res.json(categoria);
 
 }
 
-const borrarCategoria = async(req, res =response ) => {
+const borrarCategoria = async (req, res = response) => {
 
     const { id } = req.params;
-    const categoriaBorrada = await Categoria.findByIdAndUpdate( id, { estado: false }, {new: true });
+    const categoriaBorrada = await Categoria.findByIdAndUpdate(id, { estado: false }, { new: true });
 
-    res.json( categoriaBorrada );
+    res.json(categoriaBorrada);
 }
 
 
--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const fs   = require('fs');
+const fs = require('fs');
 
 const cloudinary = require('cloudinary').v2
-cloudinary.config( process.env.CLOUDINARY_URL );
+cloudinary.config(process.env.CLOUDINARY_URL);
 
 const { response } = require('express');
 const { subirArchivo } = require('../helpers');
@@ -10,14 +10,14 @@ const { subirArchivo } = require('../helpers');
 const { Usuario, Producto } = require('../models');
 
 
-const cargarArchivo = async(req, res = response) => {
+const cargarArchivo = async (req, res = response) => {
 
 
     try {
-        
+
         // txt, md
         // const nombre = await subirArchivo( req.files, ['txt','md'], 'textos' );
-        const nombre = await subirArchivo( req.files, undefined, 'imgs' );
+        const nombre = await subirArchivo(req.files, undefined, 'imgs');
         res.json({ nombre });
 
     } catch (msg) {
@@ -27,155 +27,157 @@ const cargarArchivo = async(req, res = response) => {
 }
 
 
-const actualizarImagen = async(req, res = response ) => {
+const actualizarImagen = async (req, res = response) => {
 
     const { id, coleccion } = req.params;
 
     let modelo;
 
-    switch ( coleccion ) {
+    switch (coleccion) {
         case 'usuarios':
             modelo = await Usuario.findById(id);
-            if ( !modelo ) {
+            if (!modelo) {
                 return res.status(400).json({
-                    msg: `No existe un usuario con el id ${ id }`
+                    msg: `No existe un usuario con el id ${id}`
                 });
             }
-        
-        break;
+
+            break;
 
         case 'productos':
             modelo = await Producto.findById(id);
-            if ( !modelo ) {
+            if (!modelo) {
                 return res.status(400).json({
-                    msg: `No existe un producto con el id ${ id }`
+                    msg: `No existe un producto con el id ${id}`
                 });
             }
-        
-        break;
-    
+
+            break;
+
         default:
-            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+            return res.status(500).json({ msg: 'Se me olvidó validar esto' });
     }
 
 
     // Limpiar imágenes previas
-    if ( modelo.img ) {
+    if (modelo.img) {
         // Hay que borrar la imagen del servidor
-        const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
-        if ( fs.existsSync( pathImagen ) ) {
-            fs.unlinkSync( pathImagen );
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        if (fs.existsSync(pathImagen)) {
+            fs.unlinkSync(pathImagen);
         }
     }
 
 
-    const nombre = await subirArchivo( req.files, undefined, coleccion );
+    const nombre = await subirArchivo(req.files, undefined, coleccion);
     modelo.img = nombre;
 
     await modelo.save();
 
 
-    res.json( modelo );
+    res.json(modelo);
 
 }
 
 
-const actualizarImagenCloudinary = async(req, res = response ) => {
+const actualizarImagenCloudinary = async (req, res = response) => {
 
     const { id, coleccion } = req.params;
 
     let modelo;
 
-    switch ( coleccion ) {
+    switch (coleccion) {
         case 'usuarios':
             modelo = await Usuario.findById(id);
-            if ( !modelo ) {
+            if (!modelo) {
                 return res.status(400).json({
-                    msg: `No existe un usuario con el id ${ id }`
+                    msg: `No existe un usuario con el id ${id}`
                 });
             }
-        
-        break;
+
+            break;
 
         case 'productos':
             modelo = await Producto.findById(id);
-            if ( !modelo ) {
+            if (!modelo) {
                 return res.status(400).json({
-                    msg: `No existe un producto con el id ${ id }`
+                    msg: `No existe un producto con el id ${id}`
                 });
             }
-        
-        break;
-    
+
+            break;
+
         default:
-            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+            return res.status(500).json({ msg: 'Se me olvidó validar esto' });
     }
 
 
     // Limpiar imágenes previas
-    if ( modelo.img ) {
+    if (modelo.img) {
         const nombreArr = modelo.img.split('/');
-        const nombre    = nombreArr[ nombreArr.length - 1 ];
-        const [ public_id ] = nombre.split('.');
-        cloudinary.uploader.destroy( public_id );
+        const nombre = nombreArr[nombreArr.length - 1];
+        const [public_id] = nombre.split('.');
+        cloudinary.uploader.destroy(public_id);
     }
 
 
 
+    console.log('ARCHIVO > ', req.files.archivo);
+
     const { tempFilePath } = req.files.archivo
-    const { secure_url } = await cloudinary.uploader.upload( tempFilePath );
+    const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
     modelo.img = secure_url;
 
     await modelo.save();
 
 
-    res.json( modelo );
+    res.json(modelo);
 
 }
 
-const mostrarImagen = async(req, res = response ) => {
+const mostrarImagen = async (req, res = response) => {
 
     const { id, coleccion } = req.params;
 
     let modelo;
 
-    switch ( coleccion ) {
+    switch (coleccion) {
         case 'usuarios':
             modelo = await Usuario.findById(id);
-            if ( !modelo ) {
+            if (!modelo) {
                 return res.status(400).json({
-                    msg: `No existe un usuario con el id ${ id }`
+                    msg: `No existe un usuario con el id ${id}`
                 });
             }
-        
-        break;
+
+            break;
 
         case 'productos':
             modelo = await Producto.findById(id);
-            if ( !modelo ) {
+            if (!modelo) {
                 return res.status(400).json({
-                    msg: `No existe un producto con el id ${ id }`
+                    msg: `No existe un producto con el id ${id}`
                 });
             }
-        
-        break;
-    
+
+            break;
+
         default:
-            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+            return res.status(500).json({ msg: 'Se me olvidó validar esto' });
     }
 
 
     // Limpiar imágenes previas
-    if ( modelo.img ) {
+    if (modelo.img) {
         // Hay que borrar la imagen del servidor
-        const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
-        if ( fs.existsSync( pathImagen ) ) {
-            return res.sendFile( pathImagen )
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        if (fs.existsSync(pathImagen)) {
+            return res.sendFile(pathImagen)
         }
     }
 
-    const pathImagen = path.join( __dirname, '../assets/no-image.jpg');
-    res.sendFile( pathImagen );
+    const pathImagen = path.join(__dirname, '../assets/no-image.jpg');
+    res.sendFile(pathImagen);
 }
 
 
